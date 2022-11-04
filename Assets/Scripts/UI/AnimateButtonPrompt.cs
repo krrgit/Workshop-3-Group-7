534486a9d789b97d8a318eecbb24602688e129b6
kevin <89359821+krrgit@users.noreply.github.com>
2022-11-04 00:55:00 -0700

    Added Button Prompt

---
+++ Assets/Scripts/UI/AnimateButtonPrompt.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateButtonPrompt : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    [SerializeField] private AnimationCurve animCurve;
+
+    [SerializeField] private RectTransform rect;
+
+    private float defaultSize;
+
+    void Start()
+    {
+        defaultSize = rect.localScale.x;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Animate();
+        }
+    }
+
+    public void Animate()
+    {
+        StopAllCoroutines();
+        StartCoroutine(IAnimate());
+    }
+    
+    // Open Text Bubble
+    IEnumerator IAnimate()
+    {
+        float timer = 0;
+        yield return new WaitForEndOfFrame();
+
+        while (timer < 1)
+        {
+            rect.localScale = Vector2.one * animCurve.Evaluate(timer) * defaultSize;
+            timer += Time.deltaTime/ duration;
+            yield return new WaitForEndOfFrame();
+        }
+
+        rect.localScale = Vector2.one * defaultSize;
+    }
+}
