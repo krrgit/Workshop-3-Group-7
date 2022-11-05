26c21094e9a4fceee62185f658cd4aa5b6900250
OffensiveOOF <110800344+OffensiveOOF@users.noreply.github.com>
2022-11-05 04:10:34 -0700

    Figuring out formula for checking cross section

--- Assets/Textures/Blacklight Test/Blacklight.cs
+++ Assets/Textures/Blacklight Test/Blacklight.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine.Rendering.Universal;
 using UnityEngine;
 
+
 public class Blacklight : MonoBehaviour
 {
     public Light2D blacklight;
-    public int resolution;
+    [Range(16, 512)]public int resolution;
+    public bool on;
 
     Material material;
     float outerRadius;
@@ -19,20 +21,36 @@ public class Blacklight : MonoBehaviour
 
     void Update()
     {
-        material.SetTexture("_LightMask", GenerateLightMask());
-        Debug.Log("Ran");
+        if(on) material.SetTexture("_LightMask", GenerateLightMask());
     }
 
     Texture2D GenerateLightMask(){
-        Texture2D mask = new Texture2D(resolution, resolution);
+        Texture2D mask = new Texture2D(resolution, resolution, TextureFormat.RGBA32, false);
+        float radius = outerRadius*resolution;
 
         for(int x = 0; x < resolution; x++) {
             for(int y = 0; y < resolution; y++) {
-
-                mask.SetPixel(x, y, Color.white);
+                if(checkPos(x, y, radius)){
+                    mask.SetPixel(x, y, Color.white);
+                } else {
+                    mask.SetPixel(x, y, Color.black);
+                }
             }
         }
 
+        mask.Apply();
+
         return mask;
+    }
+
+    bool checkPos(int x, int y, float radius) {
+        float xDelta = x - blacklight.transform.position.x;
+        float yDelta = y - blacklight.transform.position.y;
+
+        if ((xDelta*xDelta + yDelta*yDelta) <= radius*radius) {
+            return true;
+        }
+
+        return false;
     }
 }
