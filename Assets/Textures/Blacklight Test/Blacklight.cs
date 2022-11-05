b2b89ecf9c5e8bb5c949599ad87aab711ded2ef9
OffensiveOOF <110800344+OffensiveOOF@users.noreply.github.com>
2022-11-05 00:55:29 -0700

    blacklight attempts

---
+++ Assets/Textures/Blacklight Test/Blacklight.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blacklight : MonoBehaviour
+{
+    public Light light;
+    public Material[] mats;
+
+    public bool on;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
