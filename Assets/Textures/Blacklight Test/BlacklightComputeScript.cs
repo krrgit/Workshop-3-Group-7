e3bbba965dd98da50edcc1179f95d57339f805ca
OffensiveOOF <110800344+OffensiveOOF@users.noreply.github.com>
2022-11-05 23:00:57 -0700

    GPU will Crash - issue

--- Assets/Textures/Blacklight Test/BlacklightComputeScript.cs
+++ Assets/Textures/Blacklight Test/BlacklightComputeScript.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 public class BlacklightComputeScript : MonoBehaviour
 {
     public Light2D blacklight;
-    [Range(32, 1024)]public int resolution = 512;
+    [Range(128, 1024)]public int resolution = 512;
     public bool on = true;
 
     SpriteRenderer hiddenSprite;
@@ -20,42 +20,41 @@ public class BlacklightComputeScript : MonoBehaviour
     Vector3 lightDelta;
 
     public ComputeShader blacklightCompute;
-    public RenderTexture renderTexture;
+    RenderTexture mask;
 
     void Awake()
     {
         hiddenSprite = GetComponent<SpriteRenderer>();
-        setWidthHeightRadius();
-        GenerateLightMask();
-
-        hiddenSprite.material.SetTexture("_LightMask", renderTexture);
+        updateWidthHeightRadiusDelta();
+        GenerateMask();
     }
 
     void Update()
     {
-        setWidthHeightRadius();
-        GenerateLightMask();
-        if(on && checkRender()) hiddenSprite.material.SetTexture("_LightMask", renderTexture);
+        updateWidthHeightRadiusDelta();
+        if(on && shouldRender()) GenerateMask(); 
     }
 
-    void GenerateLightMask(){
-        renderTexture = new RenderTexture(width, height, 1);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
+    void GenerateMask(){
+        mask = new RenderTexture(width, height, 16);
+        mask.enableRandomWrite = true;
+        mask.Create();
          
-         blacklightCompute.SetTexture(0, "mask", renderTexture);
-         blacklightCompute.SetFloats("deltaPos", new float[]{lightDelta.x, lightDelta.y});
-         blacklightCompute.SetFloat("width", width);
-         blacklightCompute.SetFloat("height", height);
-         blacklightCompute.SetFloat("radius", outerRadius);
-         blacklightCompute.Dispatch(0, renderTexture.width/8, renderTexture.height/8, 1);
+        blacklightCompute.SetTexture(0, "mask", mask);
+        blacklightCompute.SetFloats("deltaPos", new float[]{lightDelta.x, lightDelta.y});
+        blacklightCompute.SetFloat("width", width);
+        blacklightCompute.SetFloat("height", height);
+        blacklightCompute.SetFloat("radius", outerRadius);
+        blacklightCompute.Dispatch(0, mask.width/8, mask.height/8, 1);
+
+         hiddenSprite.material.SetTexture("_LightMask", mask);
     }
 
-    bool checkRender() {
+    bool shouldRender() {
         return lightDelta.sqrMagnitude <= Math.Pow(outerRadius+0.6, 2);
     }
 
-    void setWidthHeightRadius() {
+    void updateWidthHeightRadiusDelta() {
         outerRadius = blacklight.pointLightOuterRadius;
         width = Convert.ToInt32(hiddenSprite.size[0]*resolution);
         height = Convert.ToInt32(hiddenSprite.size[1]*resolution);
