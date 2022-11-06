8a6c29d812003d92c4f22253ec3a888701dd10cd
OffensiveOOF <110800344+OffensiveOOF@users.noreply.github.com>
2022-11-05 18:09:44 -0700

    Blacklight is working poggies

--- Assets/Textures/Blacklight Test/Blacklight.cs
+++ Assets/Textures/Blacklight Test/Blacklight.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
 using UnityEngine.Rendering.Universal;
 using UnityEngine;
 
@@ -7,50 +9,59 @@ using UnityEngine;
 public class Blacklight : MonoBehaviour
 {
     public Light2D blacklight;
-    [Range(16, 512)]public int resolution;
+    [Range(32, 512)]public int resolution;
     public bool on;
 
-    Material material;
+    SpriteRenderer hiddenSprite;
+    int width;
+    int height;
     float outerRadius;
 
     void Awake()
     {
-        material = GetComponent<SpriteRenderer>().material;
-        outerRadius = blacklight.pointLightOuterRadius;
+        hiddenSprite = GetComponent<SpriteRenderer>();
+        setVars();
+
+        hiddenSprite.material.SetTexture("_LightMask", GenerateLightMask());
     }
 
     void Update()
     {
-        if(on) material.SetTexture("_LightMask", GenerateLightMask());
+        setVars();
+        if(on && checkRender()) hiddenSprite.material.SetTexture("_LightMask", GenerateLightMask());
     }
 
     Texture2D GenerateLightMask(){
-        Texture2D mask = new Texture2D(resolution, resolution, TextureFormat.RGBA32, false);
-        float radius = outerRadius*resolution;
+        Texture2D mask = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        Vector3 deltaPos = blacklight.transform.position - this.transform.position;
 
-        for(int x = 0; x < resolution; x++) {
-            for(int y = 0; y < resolution; y++) {
-                if(checkPos(x, y, radius)){
-                    mask.SetPixel(x, y, Color.white);
-                } else {
-                    mask.SetPixel(x, y, Color.black);
-                }
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                float c = 1 - Convert.ToSingle(checkPos(x, y, deltaPos));
+                mask.SetPixel(x, y, new Color(c, c, c, 1));
             }
         }
 
         mask.Apply();
-
         return mask;
     }
 
-    bool checkPos(int x, int y, float radius) {
-        float xDelta = x - blacklight.transform.position.x;
-        float yDelta = y - blacklight.transform.position.y;
+    bool checkRender() {
+        Vector3 deltaPos = blacklight.transform.position - this.transform.position;
+        return deltaPos.sqrMagnitude <= Math.Pow(outerRadius+0.5, 2);
+    }
 
-        if ((xDelta*xDelta + yDelta*yDelta) <= radius*radius) {
-            return true;
-        }
+    double checkPos(int x, int y, Vector3 deltaPos) {
+        Vector3 pixelPos = new Vector3(x-width/2, y-height/2, 0);
+        pixelPos = new Vector3(pixelPos.x/width, pixelPos.y/height, 0);
+        pixelPos -= deltaPos;
 
-        return false;
+        return Math.Max(0, Math.Min(1, pixelPos.sqrMagnitude/Math.Pow(outerRadius, 2)));
+    }
+
+    void setVars() {
+        outerRadius = blacklight.pointLightOuterRadius;
+        width = Convert.ToInt32(hiddenSprite.size[0]*resolution);
+        height = Convert.ToInt32(hiddenSprite.size[1]*resolution);
     }
 }
