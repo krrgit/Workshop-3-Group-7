dda99a219ea2da9656949fbacac9e127bbe2b286
JohnnyR <113726617+BURG1E@users.noreply.github.com>
2022-12-01 18:58:40 -0800

    Revert "fixing conflict error"
    This reverts commit 55f062090f97820d4fb09c4ce1d2448ea22e419f.

--- Assets/Scripts/Tools/Fishing pole/FishingPoleTool.cs
+++ Assets/Scripts/Tools/Fishing pole/FishingPoleTool.cs
@@ -20,7 +20,6 @@ public class FishingPoleTool : Tool
 
         PlayerMovement.Instance.ToggleMove(false);
         print("Use FishingPole!");
-        SoundManager.Instance.PlaycastHook();
         anim.UpdateSprite(PlayerMovement.Instance.FacingDir);
         StartCoroutine(StartFishing());
         return true;
@@ -39,9 +38,9 @@ public class FishingPoleTool : Tool
     IEnumerator StartFishing()
     {
         float waitTime = Random.Range(min, max);
-        
+
         yield return new WaitForSeconds(waitTime); 
-        SoundManager.Instance.PlaywaterSplash();
+        
         // "Catch!" animation
         
         // Start FishingMiniGame animation
@@ -72,7 +71,6 @@ public class FishingPoleTool : Tool
         {
             if(FishingSpotManager.Instance)
             {
-                SoundManager.Instance.PlaypullHook();
                 FishingSpotManager.Instance.SpotFished(true);
             }
             ProgressTracker.Instance.UpdateFishCaught();
