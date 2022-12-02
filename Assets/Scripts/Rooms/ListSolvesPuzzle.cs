f40da6ca467935fcb41374d4af52b243b977e3e1
kevin <89359821+krrgit@users.noreply.github.com>
2022-12-01 22:39:45 -0800

    list collision solves puzzle

---
+++ Assets/Scripts/Rooms/ListSolvesPuzzle.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListSolvesPuzzle : MonoBehaviour {
+    [SerializeField] private Room roomToSolve;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        ProgressTracker.Instance.SolvePuzzle(roomToSolve);
+    }
+}
